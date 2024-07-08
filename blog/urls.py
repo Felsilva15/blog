2e977fe1e608df4cc5882bc07cc4f97e6341bf3a
felsilva15 <felipedasilva1193@gmail.com>
2024-07-07 18:47:46 -0700

    Added login button to navbar and configured login/logout redirects

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
-# blog/urls.py
 from django.urls import path
 from .views import PostListView, PostDetailView
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     # Add more URLs as needed
 ]
