e0e3b1357c95924cc0d3dc9b28eaaf6beadf5688
felsilva15 <felipedasilva1193@gmail.com>
2024-07-07 19:01:20 -0700

    Fixed URL configurations and template path issues

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import PostListView, PostDetailView
-from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -8,6 +7,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='about'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     # Add more URLs as needed
 ]
