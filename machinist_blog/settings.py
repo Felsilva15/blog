ae343f025053c12d1d8f4ae2cc9ee143a3901ba1
felsilva15 <felipedasilva1193@gmail.com>
2024-07-07 20:35:16 -0700

    Update ALLOWED_HOSTS and ensure database is writable

--- machinist_blog/settings.py
+++ machinist_blog/settings.py
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'machinist_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -101,8 +101,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
