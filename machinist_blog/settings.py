fc9c9d395ec493642660e33be51c600e3ac0809f
Felipe Silva <felipe@hadrian.co>
2024-07-08 13:51:06 -0700

    Update database path to /tmp directory

--- machinist_blog/settings.py
+++ machinist_blog/settings.py
@@ -59,10 +59,11 @@ WSGI_APPLICATION = 'machinist_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Use the /tmp directory
     }
 }
 
