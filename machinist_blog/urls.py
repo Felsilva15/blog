4ab41e22160044852330a5573a3006742cc20079
felsilva15 <felipedasilva1193@gmail.com>
2024-07-07 19:03:05 -0700

    Fixed template path issues and verified directory structure

--- machinist_blog/urls.py
+++ machinist_blog/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # Include your blog app's URLs
+    path('', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
